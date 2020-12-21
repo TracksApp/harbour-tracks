01b382ae12a260a8d49214fa1706ca793a6e774c
J. Lavoie <j.lavoie@net-c.ca>
2020-12-21 03:02:29 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (38 of 38 strings)
    
    Translation: Tracks for Sailfish OS/Sailfish application
    Translate-URL: https://hosted.weblate.org/projects/harbour-tracks/harbour-tracks/de/

--- translations/harbour-tracks-de.ts
+++ translations/harbour-tracks-de.ts
@@ -5,170 +5,170 @@
     <name>AddForm</name>
     <message>
         <source>Add task</source>
-        <translation type="unfinished"></translation>
+        <translation>Aufgabe hinzufügen</translation>
     </message>
     <message>
         <source>Task description</source>
-        <translation type="unfinished"></translation>
+        <translation>Aufgabenbeschreibung</translation>
     </message>
     <message>
         <source>Note</source>
-        <translation type="unfinished"></translation>
+        <translation>Notiz</translation>
     </message>
     <message>
         <source>Context</source>
-        <translation type="unfinished"></translation>
+        <translation>Kontext</translation>
     </message>
     <message>
         <source>Project</source>
-        <translation type="unfinished"></translation>
+        <translation>Projekt</translation>
     </message>
     <message>
         <source>Due date</source>
-        <translation type="unfinished"></translation>
+        <translation>Fälligkeitsdatum</translation>
     </message>
     <message>
         <source>Select</source>
-        <translation type="unfinished"></translation>
+        <translation>Auswählen</translation>
     </message>
     <message>
         <source>Show from</source>
-        <translation type="unfinished"></translation>
+        <translation>Anzeigen ab</translation>
     </message>
     <message>
         <source>Tracks task added</source>
-        <translation type="unfinished"></translation>
+        <translation>Tracks-Aufgabe hinzugefügt</translation>
     </message>
     <message>
         <source>Tracks task adding failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Hinzufügen von Tracks-Aufgabe fehlgeschlagen</translation>
     </message>
     <message>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>Keine</translation>
     </message>
     <message>
         <source>Task %1 added to context %2.</source>
-        <translation type="unfinished"></translation>
+        <translation>Aufgabe %1 wurde zu Kontext %2 hinzugefügt.</translation>
     </message>
     <message>
         <source>Adding task %1 failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Fehler beim Hinzufügen von Aufgabe %1.</translation>
     </message>
     <message>
         <source>Tag</source>
-        <translation type="unfinished"></translation>
+        <translation>Markierung</translation>
     </message>
 </context>
 <context>
     <name>ContextList</name>
     <message>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Einstellungen</translation>
     </message>
     <message>
         <source>Add task</source>
-        <translation type="unfinished"></translation>
+        <translation>Aufgabe hinzufügen</translation>
     </message>
     <message>
         <source>Refresh</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktualisieren</translation>
     </message>
     <message>
         <source>Contexts</source>
-        <translation type="unfinished"></translation>
+        <translation>Kontexte</translation>
     </message>
     <message>
         <source>Nothing here</source>
-        <translation type="unfinished"></translation>
+        <translation>Nichts hier</translation>
     </message>
     <message>
         <source>Check your settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Überprüfen Sie die Einstellungen</translation>
     </message>
     <message>
         <source>Scroll to top</source>
-        <translation type="unfinished"></translation>
+        <translation>Nach oben blättern</translation>
     </message>
 </context>
 <context>
     <name>CoverPage</name>
     <message>
         <source>Tracks</source>
-        <translation type="unfinished"></translation>
+        <translation>Nachverfolgung</translation>
     </message>
 </context>
 <context>
     <name>SettingsPage</name>
     <message>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Speichern</translation>
     </message>
     <message>
         <source>Tracks base URL</source>
-        <translation type="unfinished"></translation>
+        <translation>Tracks-Basis-URL</translation>
     </message>
     <message>
         <source>Username</source>
-        <translation type="unfinished"></translation>
+        <translation>Benutzername</translation>
     </message>
     <message>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Passwort</translation>
     </message>
 </context>
 <context>
     <name>TaskList</name>
     <message>
         <source>Refresh</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktualisieren</translation>
     </message>
     <message>
         <source>Nothing here</source>
-        <translation type="unfinished"></translation>
+        <translation>Nichts hier</translation>
     </message>
     <message>
         <source>Check your settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Überprüfen Sie die Einstellungen</translation>
     </message>
     <message>
         <source>Marking as done</source>
-        <translation type="unfinished"></translation>
+        <translation>Als erledigt markieren</translation>
     </message>
     <message>
         <source>Task %1 set done.</source>
-        <translation type="unfinished"></translation>
+        <translation>Aufgabe %1 als erledigt markiert.</translation>
     </message>
     <message>
         <source>Tracks task set done</source>
-        <translation type="unfinished"></translation>
+        <translation>Tracks-Aufgabe als erledigt markiert</translation>
     </message>
     <message>
         <source>Setting task %1 done failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Markieren von Aufgabe %1 fehlgeschlagen.</translation>
     </message>
     <message>
         <source>Tracks task setting failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Einstellen der Tracks-Aufgabe fehlgeschlagen</translation>
     </message>
     <message>
         <source>Scroll to top</source>
-        <translation type="unfinished"></translation>
+        <translation>Nach oben blättern</translation>
     </message>
     <message>
         <source>Actions in %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktionen in %1</translation>
     </message>
     <message>
         <source>Add task</source>
-        <translation type="unfinished"></translation>
+        <translation>Aufgabe hinzufügen</translation>
     </message>
 </context>
 <context>
     <name>harbour-tracks</name>
     <message>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>Keine</translation>
     </message>
 </context>
 </TS>
